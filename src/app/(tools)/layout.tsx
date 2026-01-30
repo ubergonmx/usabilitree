7a024b0b28c1fc93ae0784cf21742dd427cd02e8
aaron <aaron_palpallatoc@dlsu.edu.ph>
2026-01-30 16:13:01 +0800

    feat: add robots metadata for SEO optimization and create robots route

--- src/app/(tools)/layout.tsx
+++ src/app/(tools)/layout.tsx
@@ -1,5 +1,13 @@
 import PageTheme from "@/providers/page-theme";
+import { Metadata } from "next";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 const ToolLayout = ({ children }: { children: ReactNode }) => {
   return <PageTheme>{children}</PageTheme>;
